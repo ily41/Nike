691576c59d240ac24dcf3876e0ebed617aa841ba
ily41 <rilyayi@std.beu.edu.az>
2025-07-20 04:10:47 +0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+    <video playsinline="playsinline" class="vjs-tech" aria-label="Looping video with no sound" id="vjs_video_561_html5_api" tabindex="-1" role="application" preload="auto" src="blob:https://www.nike.com/e002a076-a9a2-425f-b1b2-e9c8d0abe8e7"></video>
+    </>
+  )
+}
+
+export default App
